5cfa81a69fa7c5f7e3a8c330e9c0a73c59293243
佐藤千草 <a81809@MacBook-Air.local>
2023-01-31 08:06:44 +0900

    モックデータの修正

--- src/stores/favorite.js
+++ src/stores/favorite.js
@@ -5,30 +5,30 @@ export const useFavorite = defineStore('favorite-store', {
     return {
       favorites: [
         {
-          author: null,
+          author: 'authorA',
           content: null,
           description:
-            '7月スタートの火曜ドラマ『ユニコーンに乗って』の主演に永野芽郁が決定!!永野自身も好きなドラマ枠での出演が決定した喜びを語る!!さらに、本作で初挑戦となるCEO役のための準備していることとは!?魅力満載のスペシャルインタビュー!!■番組概要７月期の火曜ドラマは、大北はるかが脚本を手掛ける完全オリジナルストーリー『...',
+          'ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示',
           id: '180d224c5af31',
           publishedAt: '2022-05-17T08:00:04Z',
           source: { id: null, name: 'YouTube' },
           title:
-            '【新火曜ドラマ】永野芽郁 初のCEO役に挑戦!! スペシャルインタビュー!!『ユニコーンに乗って』【TBS】 - TBS公式 YouTuboo',
-          url: 'https://www.youtube.com/watch?v=FpvJfVS3W6o',
+          'タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル',
+      url: 'https://www.youtube.com/watch?v=FpvJfVS3W6o',
           urlToImage: 'https://i.ytimg.com/vi/FpvJfVS3W6o/maxresdefault.jpg',
           needsClip: true,
         },
         {
-          author: '読売新聞オンライン',
+          author: 'authorB',
           content: '',
           description:
-            '【キーウ＝川上大介】８日の爆発で一部崩落が起きたロシア本土とウクライナ南部クリミア半島を結ぶ「クリミア大橋」（全長約１９キロ・メートル）について、タス通信は、８日夕に車両と列車の通行が限定的に再開したと伝えた。英国防省',
+          'ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示',
           id: '183bdbdf74b1e5',
           publishedAt: '2022-10-09T11:19:00Z',
           source: { id: null, name: 'Yomiuri.co.jp' },
           title:
-            '１９ｋｍのクリミア大橋崩落、ウクライナ情報機関が関与の報道…ゼレンスキー氏「未来は快晴」 - 読売新聞オンライン',
-          url: 'https://www.yomiuri.co.jp/world/20221009-OYT1T50150/',
+          'タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル',
+      url: 'https://www.yomiuri.co.jp/world/20221009-OYT1T50150/',
           urlToImage:
             'https://www.yomiuri.co.jp/media/2022/10/20221009-OYT1I50090-T.jpg?type=ogp',
           needsClip: true,
--- src/stores/timeline.js
+++ src/stores/timeline.js
@@ -7,15 +7,15 @@ export const useTimeline = defineStore('timeline-store', {
         {
           comment: 'コメントコメントコメント',
           article: {
-            author: 'スポニチ Sponichi Annex',
+            author: 'AuthorB',
             content: '',
             description:
-              '56号を記録した村上について「まだ5年目の選手ですよね。いやーもう素晴らしいですね」と称賛した。さらに令和初の3冠王に輝いたことについても「今までになかったスケ… - スポーツニッポン新聞社の公式サイト',
+              'ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示',
             id: '180d224c5af31',
             publishedAt: '2022-05-17T08:00:04Z',
             source: { id: null, name: 'YouTube' },
             title:
-              '松井秀喜氏がヤクルト・村上を称賛「今までになかったスケールの選手」 打撃フォームも独自分析 - スポニチアネックス Sponichi Annex',
+              'タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル',
             url: 'https://www.sponichi.co.jp/baseball/news/2022/10/12/kiji/20221012s00001173477000c.html',
             urlToImage:
               'https://www.sponichi.co.jp/baseball/news/2022/10/12/jpeg/20221012s00001173484000p_view.jpg',
@@ -25,15 +25,15 @@ export const useTimeline = defineStore('timeline-store', {
         {
           comment: 'コメントコメントコメント',
           article: {
-            author: '読売新聞オンライン',
+            author: 'authorA',
             content: '',
             description:
-              '【キーウ＝川上大介】８日の爆発で一部崩落が起きたロシア本土とウクライナ南部クリミア半島を結ぶ「クリミア大橋」（全長約１９キロ・メートル）について、タス通信は、８日夕に車両と列車の通行が限定的に再開したと伝えた。英国防省',
+              'ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示ニュース情報をここに表示',
             id: '183bdbdf74b1e5',
             publishedAt: '2022-10-09T11:19:00Z',
             source: { id: null, name: 'Yomiuri.co.jp' },
             title:
-              '１９ｋｍのクリミア大橋崩落、ウクライナ情報機関が関与の報道…ゼレンスキー氏「未来は快晴」 - 読売新聞オンライン',
+              'タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル',
             url: 'https://www.yomiuri.co.jp/world/20221009-OYT1T50150/',
             urlToImage:
               'https://www.yomiuri.co.jp/media/2022/10/20221009-OYT1I50090-T.jpg?type=ogp',
